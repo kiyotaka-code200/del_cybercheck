6e44999841365dea30d37ebb908363d4fe722c64
delson <delson@localhost.localdomain>
2026-04-29 12:35:53 +0100

    Finalisation du Frontend et Backend

--- audit/urls.py
+++ audit/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'), 
-    path('resultats/', views.resultats, name='resultats'),
+    path('', views.home, name='home'),
 ]
