39933017bade1409d33eb7fea86e22610b0cb241
delson <delson@localhost.localdomain>
2026-04-29 12:27:57 +0100

    Finalisation du Frontend et Backend

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('audit.urls')), 
+    path('', include('audit.urls')), # Le '' signifie "page d'accueil"
 ]
